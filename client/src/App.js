28be2d579fc8ac37f9bd09599e5f89b0a608c007
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-01-29 19:29:49 -0500

    added header section and styled for mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Navigation from './components/Navigation'
+import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,81 @@
+import {
+  FaFlickr,
+  FaFacebookSquare,
+  FaYoutube,
+  FaEnvelopeSquare,
+  FaArrowAltCircleDown,
+} from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>A Brief Overview</h1>
+        <p>
+          Hi! We are a <span className='special'> family owned business</span>
+          &nbsp; specializing in automotive interiors and convertible tops&nbsp;
+          <span className='special'>since 1968</span>. Have a look around!
+        </p>
+
+        <h1>Visit Us</h1>
+        <p>
+          <span className='special'>Location:</span> 2040 Northeast 154 Street,
+          North Miami, Florida 33162 USA
+        </p>
+        <p>
+          <span className='special'>Business Hours:</span> 7 AM - 5 PM Monday -
+          Friday
+        </p>
+
+        <h1>Keep in Touch</h1>
+        <ul>
+          <li>
+            <FaFlickr className='keep-in-touch-icon' /> View our Flickr photo
+            gallery&nbsp;
+            <a
+              href='https://www.flickr.com/photos/shamrockride/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              here
+            </a>
+          </li>
+          <li>
+            <FaFacebookSquare className='keep-in-touch-icon' /> Follow us on
+            Facebook&nbsp;
+            <a
+              href='https://www.facebook.com/shamrocktrim/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              here
+            </a>
+          </li>
+          <li>
+            <FaYoutube className='keep-in-touch-icon' /> Watch our YouTube
+            videos&nbsp;
+            <a
+              href='https://www.youtube.com/c/ShamrockTrim/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              here
+            </a>
+          </li>
+          <li>
+            <FaEnvelopeSquare className='keep-in-touch-icon' /> Contact us&nbsp;
+            <a href='#contact'>here</a>
+          </li>
+        </ul>
+
+        <div id='scroll-down-button-wrapper'>
+          <a href='#about-us' id='scroll-down-button'>
+            <FaArrowAltCircleDown  />
+          </a>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
