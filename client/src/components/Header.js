9dbf1fe2be3448b6cc7fb77d5928f3bb77d9e8f3
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-01-31 19:00:39 -0500

    added header images

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,28 +9,132 @@ import ImagesCarousel from './ImagesCarousel';
 
 const imagesList = [
   {
-    image: './assets/car-test/car1.jpg',
-    title: 'Car 1',
+    image: './assets/header-images/header-1.jpg',
+    title: 'Dan McPherson, founder of Shamrock Auto Trim',
   },
   {
-    image: './assets/car-test/car2.jpg',
-    title: 'Car 2',
+    image: './assets/header-images/header-2.jpeg',
+    title: 'A family business since 1968',
   },
   {
-    image: './assets/car-test/car3.jpg',
-    title: 'Car 3',
+    image: './assets/header-images/header-3.jpeg',
+    title: `This Mini Cooper's interior did not match the exterior`,
   },
   {
-    image: './assets/car-test/car4.jpg',
-    title: 'Car 4',
+    image: './assets/header-images/header-4.jpg',
+    title: 'Original plain black Mini Cooper interior before we transformed it',
   },
   {
-    image: './assets/car-test/car5.jpg',
-    title: 'Car 5',
+    image: './assets/header-images/header-5.jpg',
+    title: 'Converted pieces to red carbon fiber vinyl',
   },
   {
-    image: './assets/car-test/car6.jpg',
-    title: 'Car 6',
+    image: './assets/header-images/header-6.jpg',
+    title: 'Installed new leather upholstery and recovered the door panels',
+  },
+  {
+    image: './assets/header-images/header-7.jpg',
+    title: 'Closeup of transformed Ferrari',
+  },
+  {
+    image: './assets/header-images/header-8.jpg',
+    title: 'NBA star Eddie Jones restoration including new convertible top replacement, leather seats, carpet and trunk',
+  },
+  {
+    image: './assets/header-images/header-9.jpg',
+    title: 'Shamrock fabricated new door panels',
+  },
+  {
+    image: './assets/header-images/header-10.jpg',
+    title: 'Shamrock recovered the new panels with leather',
+  },
+  {
+    image: './assets/header-images/header-11.jpg',
+    title: 'We fabricated new trunk panels and covered them with leather',
+  },
+  {
+    image: './assets/header-images/header-12.jpg',
+    title: 'We recovered the panels with leather and matched seat style and appearance',
+  },
+  {
+    image: './assets/header-images/header-13.jpg',
+    title: 'We built new leather upholstery with a green accent stitch',
+  },
+  {
+    image: './assets/header-images/header-14.jpg',
+    title: 'Mustang convertible top installation',
+  },
+  {
+    image: './assets/header-images/header-15.jpg',
+    title: 'This customer found this Mercedes in a barn and brought it to us for a complete interior',
+  },
+  {
+    image: './assets/header-images/header-16.jpg',
+    title: 'We also builds the shamrockride.com heat shields',
+  },
+  {
+    image: './assets/header-images/header-17.jpg',
+    title: 'We replaced the leather but also works with vinyl and cloth',
+  },
+  {
+    image: './assets/header-images/header-18.jpg',
+    title: 'We repaired the seat frame, replaced the seat foam, and built new upholstery',
+  },
+  {
+    image: './assets/header-images/header-19.jpg',
+    title: 'We build the the stay fast parking cover for the Lamborghini Murcielago',
+  },
+  {
+    image: './assets/header-images/header-20.jpg',
+    title: 'A picture from the past!',
+  },
+  {
+    image: './assets/header-images/header-21.jpg',
+    title: 'New tonneau cover',
+  },
+  {
+    image: './assets/header-images/header-22.jpg',
+    title: 'New leather upholstery',
+  },
+  {
+    image: './assets/header-images/header-23.jpg',
+    title: 'Custom motorcycle seat',
+  },
+  {
+    image: './assets/header-images/header-24.jpg',
+    title: 'Boat instrument panel fabrication',
+  },
+  {
+    image: './assets/header-images/header-25.jpg',
+    title: 'Antique bench before restoration',
+  },
+  {
+    image: './assets/header-images/header-26.jpg',
+    title: 'Antique bench after restoration',
+  },
+  {
+    image: './assets/header-images/header-27.jpeg',
+    title: `One of the many cars of Universal's 2Fast2Furious movie cars we built`,
+  },
+  {
+    image: './assets/header-images/header-28.jpg',
+    title: 'Corvette convertible top installation',
+  },
+  {
+    image: './assets/header-images/header-29.jpg',
+    title: 'Antique Mercedes convertible interior restoration',
+  },
+  {
+    image: './assets/header-images/header-30.jpg',
+    title: 'New upholstery and loop carpet installation',
+  },
+  {
+    image: './assets/header-images/header-31.jpg',
+    title: 'Lamborghini leather upholstery',
+  },
+  {
+    image: './assets/header-images/header-32.jpg',
+    title: 'Honda armrest cover restoration',
   },
 ];
 
@@ -98,7 +202,7 @@ const Header = () => {
 
         <div id='scroll-down-button-wrapper'>
           <a href='#about' id='scroll-down-button'>
-            <FaArrowAltCircleDown  />
+            <FaArrowAltCircleDown />
           </a>
         </div>
       </header>
