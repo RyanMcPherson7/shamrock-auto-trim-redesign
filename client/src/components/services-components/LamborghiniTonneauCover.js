c7903b7086dabf775406cae49cc3f4c11788b899
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-02-06 23:32:11 -0500

    updated lambo wording and some hover effects

--- client/src/components/services-components/LamborghiniTonneauCover.js
+++ client/src/components/services-components/LamborghiniTonneauCover.js
@@ -44,55 +44,40 @@ const LamborghiniTonneauCover = () => {
 
           <div className='quote-block'>
             <p className='quote-paragraph'>
-              "When Lamborghini built the Murcielago Roadster, they had a single
-              mission, build it fast. With that in mind, their main focus was
-              engine performance. Lamborghini ran out of room in the engine
-              compartment for a traditional folding convertible top. They
-              decided to build the convertible with a folding targa type top
-              that fit in a small compartment in the front trunk. The main
-              problem was that&nbsp;
-              <span className='special'>
-                you needed a Lamborghini technician to follow you
-              </span>
-              &nbsp; in his car to fold the top.
+              "When Lamborghini built the Murcielago Roadster, they had 1
+              mission: build it fast! To do so, they focused entirely on engine
+              performance which left no room in the engine compartment for a
+              traditional folding convertible top. Their solution was to install
+              a complicated Targa type top that only technicians knew how to
+              operate. It was simply impractical.
             </p>
             <p className='quote-paragraph'>
-              "There were a few companies that tried to build a carbon fiber
-              roof, but when customers gave deposits, they usually lost their
-              deposit money and received nothing in return. Irv David, owner of
-              Prestige Imports, now Lamborghini of Miami and Palm Beach,
-              presented Shamrock with a challenge. Mr. David had a customer that
-              had taken delivery of a brand new 2006 Murcielago and wanted to
-              return the car as he could not work the factory top. Mr. David
-              told us that the factory said there was no other top for the car.
-              The first thing we realized was that we didn't want to drill into
-              a brand new 2006 Lamborghini. Therefore we had to make it work
-              without altering anything on the car. The original top had snaps
-              in the engine compartment and a lift snap on the side. We made a
-              pattern and kept changing it until we were satisfied with the
-              design. Mr. David returned with the customer and they could not
-              believe that&nbsp;
-              <span className='special'>
-                we found a solution for the problem whereas, Lamborghini could
-                not
-              </span>
-              . It was a proud moment for our business and family. We agreed to
-              give Mr. David's Lamborghini dealership the exclusive right to
-              sell our cover at his dealerships as well as online. We are now
-              allowed to sell directly to the public."
+              Irv David, owner of Lamborghini of Miami and Palm Beach, presented
+              Shamrock with this same challenge. He told us about a story of a
+              customer requesting to return his new 2006 Murcielago since he
+              couldn't operate the convertible top. We went straight to the
+              drawing board. We opted to not drill holes into the brand new car
+              and instead took advantage of the snaps in the engine compartment
+              and the lift snap on the side. After many iterations of pattern
+              designs, we landed on the one! Mr. David presented his customer
+              with our custom top and he couldn't believe it. We solved this
+              challenge, whereas Lamborghini could not. It was a proud moment
+              for our business and family. We agreed to give Mr. David's
+              Lamborghini dealership the exclusive right to sell our cover at
+              his dealerships and online. Today, we are now allowed to sell
+              directly to the public.
             </p>
             <p className='quote-paragraph'>
-              We build the cover using Hartz Stayfast black cloth. This material
-              is a three ply composite made of an acrylic weave outer fabric, a
-              rubber inner layer, and a cotton lining. This material is used as
-              original equipment for the Bentley Azure and the Lotus Elise
-              convertible tops. This material is waterproof. The cover has
-              removable support inserts as it can be folded to fit in the front
-              boot area. A soft cloth on the back side of the cover provides an
-              extra layer of protection to shield the paint. We have also placed
-              a soft pad to protect the front mirrors and paint from direct
-              contact with the front straps. The cover can be installed in
-              minutes!"
+              We build the covers using Hartz Stayfast black cloth, a 3-ply
+              composite material made of an acrylic woven outer fabric, a rubber
+              inner layer, and a cotton lining. This material is used for
+              building the Bentley Azure and the Lotus Elise convertible tops
+              and is also water proof. Our cover has removeable support inserts
+              so it can be folded to fit in the front boot area. A soft cloth on
+              the cover provides a layer of protection to shield the paint.
+              There's also a soft pad to protect the from mirrors and paint from
+              direct contact with the front straps. Our cover can be installed
+              in minutes!"
             </p>
             <p className='quote-author'>
               - <span className='special'>Randy McPherson</span>, head of
