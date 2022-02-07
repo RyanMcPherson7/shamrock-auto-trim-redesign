dd17f27a01836a4b16a2be8495fe847fd9bc5243
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-02-06 22:44:12 -0500

    adjsuted wording of headliner section

--- client/src/components/services-components/Headliners.js
+++ client/src/components/services-components/Headliners.js
@@ -25,32 +25,31 @@ const Headliners = () => {
           <div className='quote-block'>
             <p className='quote-paragraph'>
               "Almost every automobile, from a Kia Rio to a Bentley Continental
-              GT has something in common. The car manufacturers have decided to
-              install a hard headliner board, which is made out of cardboard or
-              laminated fiberglass. The next step is to cover this board with a
-              finished material that matches the interior. They can use cloth,
-              vinyl, suede or leather. In order to secure this material and
-              provide a good surface to glue to, they laminate the finished
-              material to a piece of foam. Then they glue the two pieces to the
-              hard headliner board. Over time, the foam base that is between the
-              finished material and the headliner board breaks down from heat
-              and turns to dust. No one can secure the finished material to the
-              broken down foam. The solution is to remove the trim panels and
-              carefully take the hard headliner board out of the car. Then the
-              board is cleaned and prepared with solvent. The next step is to
-              glue new finished material and foam to the hard headliner board.
+              GT has something in common: a hard headliner board made out of
+              cardboard or laminated fiberglass. This board is covered with a
+              finished material (cloth, vinyl, suede, or leather) that matches
+              the interior. This material is then laminated to a piece of foam
+              so that it can be easily glued to the roof of your car. Over time,
+              the foam base holding the finished material and headliner board
+              together breaks down from heat and turns to dust. As a result, the
+              finished material is no longer securely connected to the
+              board.&nbsp;
+              <span className='special'>
+                Shamrock Auto Trim solves this problem
+              </span>
+              &nbsp;by carefully removing your car's hard headliner board,
+              cleaning it with solvent, covering it with new material, and
+              reinstalling the final product.
             </p>
             <p className='quote-paragraph'>
-              "Shamrock stocks the <span className='special'>new materials</span>
-              &nbsp; and in most cases, you can&nbsp;
+              We stock the newest materials. In most cases, you can&nbsp;
               <span className='special'>
-                drop off your car in the morning and pick it up in the afternoon
+                drop your car off in the morning and pick it up in the afternoon
               </span>
-              . Another option is to come in the morning, wait 20 minutes in our
-              air conditioned office with internet, and have us remove the hard
-              headliner board. Leave with your car and then you will return in
-              the afternoon for us to install your hard headliner board with the
-              new material."
+              . If you're in a hurry, you can wait 20 minutes in our comfortable
+              office for us to remove the board. Afterward, you can simply leave
+              with your car, go about your day, and return in the afternoon for
+              us to install your newly built headliner board."
             </p>
             <p className='quote-author'>
               - <span className='special'>Randy McPherson</span>, head of
