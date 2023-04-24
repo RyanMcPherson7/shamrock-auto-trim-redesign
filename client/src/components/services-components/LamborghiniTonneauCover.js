0310c335f3aeb64d8c83b489fb570c4832d6a501
Ryan McPherson <ryan7mcpherson@gmail.com>
2023-04-24 11:51:45 -0400

    update price of parking cover from 900 to 1200

--- client/src/components/services-components/LamborghiniTonneauCover.js
+++ client/src/components/services-components/LamborghiniTonneauCover.js
@@ -37,7 +37,7 @@ const LamborghiniTonneauCover = () => {
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                Buy Parking Cover ($900.00)
+                Buy Parking Cover ($1200.00)
               </a>
             </button>
           </div>
